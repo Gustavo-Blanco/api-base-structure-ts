701c310dea0af3927a3e67fe20fc6b7ec32b424f
Gustavo Blanco Ticona <gustavo.blanco@tecsup.edu.pe>
2022-01-20 12:12:18 -0500

    env example

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+console.log(process.env.DATABASE_URL);
 
 //routes
 app.use("/api/v1", api);
